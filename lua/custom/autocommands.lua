99266d706dfc88b8f035e96d2d229cf132ad1b47
heinrob <github@petrosh.de>
2026-04-15 21:55:06 +0200

    restore last cursor position

--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -9,3 +9,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
+
+-- restore last cursor position when opening a buffer
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Restore last cursor position when opening a buffer',
+  group = vim.api.nvim_create_augroup('my-editor-position', { clear = true }),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
+  end,
+})
